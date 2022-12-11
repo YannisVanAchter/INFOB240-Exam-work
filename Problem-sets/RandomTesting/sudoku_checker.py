458f341cc60b75fe94bd8f7df2a0071b722854ce
Yannis Van Achter <yannis.van.achter@gmail.com>
2022-12-11 10:44:06 +0100

    Black formating + solving logical bug

--- Problem-sets/RandomTesting/sudoku_checker.py
+++ Problem-sets/RandomTesting/sudoku_checker.py
@@ -18,7 +18,7 @@
 # If the sanity checks pass, your code should return True if all of
 # the following hold, and False otherwise:
 #
-# - each number in the range 1..9 occurs only once in each row 
+# - each number in the range 1..9 occurs only once in each row
 #
 # - each number in the range 1..9 occurs only once in each column
 #
@@ -30,7 +30,7 @@
 #
 # 5 3 4 | 6 7 8 | 9 1 2
 # 6 7 2 | 1 9 5 | 3 4 8
-# 1 9 8 | 3 4 2 | 5 6 7 
+# 1 9 8 | 3 4 2 | 5 6 7
 # ---------------------
 # 8 5 9 | 7 6 1 | 4 2 3
 # 4 2 6 | 8 5 3 | 7 9 1
@@ -39,127 +39,150 @@
 # 9 6 1 | 5 3 7 | 0 0 0
 # 2 8 7 | 4 1 9 | 0 0 0
 # 3 4 5 | 2 8 6 | 0 0 0
-# 
+#
 # Please keep in mind that a valid grid (i.e., one for which your
 # function returns True) may contain 0 multiple times in a row,
 # column, or sub-grid. Here we are using 0 to represent an element of
 # the Sudoku grid that the player has not yet filled in.
 
 # check_sudoku should return None
-ill_formed = [[5,3,4,6,7,8,9,1,2],
-              [6,7,2,1,9,5,3,4,8],
-              [1,9,8,3,4,2,5,6,7],
-              [8,5,9,7,6,1,4,2,3],
-              [4,2,6,8,5,3,7,9],  # <---
-              [7,1,3,9,2,4,8,5,6],
-              [9,6,1,5,3,7,2,8,4],
-              [2,8,7,4,1,9,6,3,5],
-              [3,4,5,2,8,6,1,7,9]]
+ill_formed = [
+    [5, 3, 4, 6, 7, 8, 9, 1, 2],
+    [6, 7, 2, 1, 9, 5, 3, 4, 8],
+    [1, 9, 8, 3, 4, 2, 5, 6, 7],
+    [8, 5, 9, 7, 6, 1, 4, 2, 3],
+    [4, 2, 6, 8, 5, 3, 7, 9],  # <---
+    [7, 1, 3, 9, 2, 4, 8, 5, 6],
+    [9, 6, 1, 5, 3, 7, 2, 8, 4],
+    [2, 8, 7, 4, 1, 9, 6, 3, 5],
+    [3, 4, 5, 2, 8, 6, 1, 7, 9],
+]
 
 # check_sudoku should return True
-valid = [[5,3,4, 6,7,8, 9,1,2],
-         [6,7,2, 1,9,5, 3,4,8],
-         [1,9,8, 3,4,2, 5,6,7],
-         # ------------------
-         [8,5,9, 7,6,1, 4,2,3],
-         [4,2,6, 8,5,3, 7,9,1],
-         [7,1,3, 9,2,4, 8,5,6],
-         # ------------------
-         [9,6,1, 5,3,7, 2,8,4],
-         [2,8,7, 4,1,9, 6,3,5],
-         [3,4,5, 2,8,6, 1,7,9]]
+valid = [
+    [5, 3, 4, 6, 7, 8, 9, 1, 2],
+    [6, 7, 2, 1, 9, 5, 3, 4, 8],
+    [1, 9, 8, 3, 4, 2, 5, 6, 7],
+    # ------------------
+    [8, 5, 9, 7, 6, 1, 4, 2, 3],
+    [4, 2, 6, 8, 5, 3, 7, 9, 1],
+    [7, 1, 3, 9, 2, 4, 8, 5, 6],
+    # ------------------
+    [9, 6, 1, 5, 3, 7, 2, 8, 4],
+    [2, 8, 7, 4, 1, 9, 6, 3, 5],
+    [3, 4, 5, 2, 8, 6, 1, 7, 9],
+]
 
 # check_sudoku should return False
-invalid = [[5,3,4, 6,7,8, 9,1,2], #Two eight in second sub-grid (line 1: id 3 and 3: id 2)
-           [6,7,2, 1,9,5, 3,4,8],
-           [1,9,8, 3,8,2, 5,6,7],
-           # ------------------
-           [8,5,9, 7,6,1, 4,2,3],
-           [4,2,6, 8,5,3, 7,9,1],
-           [7,1,3, 9,2,4, 8,5,6],
-           # ------------------
-           [9,6,1, 5,3,7, 2,8,4],
-           [2,8,7, 4,1,9, 6,3,5],
-           [3,4,5, 2,8,6, 1,7,9]]
+invalid = [
+    [
+        5,
+        3,
+        4,
+        6,
+        7,
+        8,
+        9,
+        1,
+        2,
+    ],  # Two eight in second sub-grid (line 1: id 3 and 3: id 2)
+    [6, 7, 2, 1, 9, 5, 3, 4, 8],
+    [1, 9, 8, 3, 8, 2, 5, 6, 7],
+    # ------------------
+    [8, 5, 9, 7, 6, 1, 4, 2, 3],
+    [4, 2, 6, 8, 5, 3, 7, 9, 1],
+    [7, 1, 3, 9, 2, 4, 8, 5, 6],
+    # ------------------
+    [9, 6, 1, 5, 3, 7, 2, 8, 4],
+    [2, 8, 7, 4, 1, 9, 6, 3, 5],
+    [3, 4, 5, 2, 8, 6, 1, 7, 9],
+]
 
 # check_sudoku should return True
-easy = [[2,9,0, 0,0,0, 0,7,0],
-        [3,0,6, 0,0,8, 4,0,0],
-        [8,0,0, 0,4,0, 0,0,2],
-        # ------------------
-        [0,2,0,0,3,1,0,0,7],
-        [0,0,0,0,8,0,0,0,0],
-        [1,0,0,9,5,0,0,6,0],
-        # ------------------
-        [7,0,0, 0,9,0, 0,0,1],
-        [0,0,1, 2,0,0, 3,0,6],
-        [0,3,0, 0,0,0, 0,5,9]]
+easy = [
+    [2, 9, 0, 0, 0, 0, 0, 7, 0],
+    [3, 0, 6, 0, 0, 8, 4, 0, 0],
+    [8, 0, 0, 0, 4, 0, 0, 0, 2],
+    # ------------------
+    [0, 2, 0, 0, 3, 1, 0, 0, 7],
+    [0, 0, 0, 0, 8, 0, 0, 0, 0],
+    [1, 0, 0, 9, 5, 0, 0, 6, 0],
+    # ------------------
+    [7, 0, 0, 0, 9, 0, 0, 0, 1],
+    [0, 0, 1, 2, 0, 0, 3, 0, 6],
+    [0, 3, 0, 0, 0, 0, 0, 5, 9],
+]
 
 # check_sudoku should return True
-hard = [[1,0,0, 0,0,7, 0,9,0],
-        [0,3,0, 0,2,0, 0,0,8],
-        [0,0,9, 6,0,0, 5,0,0],
-        # ------------------
-        [0,0,5, 3,0,0, 9,0,0],
-        [0,1,0, 0,8,0, 0,0,2],
-        [6,0,0, 0,0,4, 0,0,0],
-        # ------------------
-        [3,0,0, 0,0,0, 0,1,0],
-        [0,4,0, 0,0,0, 0,0,7],
-        [0,0,7, 0,0,0, 3,0,0]]
+hard = [
+    [1, 0, 0, 0, 0, 7, 0, 9, 0],
+    [0, 3, 0, 0, 2, 0, 0, 0, 8],
+    [0, 0, 9, 6, 0, 0, 5, 0, 0],
+    # ------------------
+    [0, 0, 5, 3, 0, 0, 9, 0, 0],
+    [0, 1, 0, 0, 8, 0, 0, 0, 2],
+    [6, 0, 0, 0, 0, 4, 0, 0, 0],
+    # ------------------
+    [3, 0, 0, 0, 0, 0, 0, 1, 0],
+    [0, 4, 0, 0, 0, 0, 0, 0, 7],
+    [0, 0, 7, 0, 0, 0, 3, 0, 0],
+]
 
-def get_column(column, row, grid):
-    to_return = []
-    for r in range(9):
-        for c in range(9):
-            if c == column and r != row :
-                to_return.append(grid[r][c])
-            
-    return to_return
 
 def check_sudoku(grid):
-    if not isinstance(grid, list) and len(grid) != 9: 
+    if not isinstance(grid, list) and len(grid) != 9:
         return None
-    
+
     # general testing on each row
     for row in grid:
         if not isinstance(row, list) or len(row) != 9:
             return None
-        d = set() # make sure there is only on occurence of each element
+        d = set()  # make sure there is only on occurence of each element
         for element in row:
-            if not 0 <= element <= 9:
+            if not isinstance(element, int):
                 return None
-            if element in d:
+            if not 0 <= element <= 9:
+                return False
+            if element != 0 and element in d:
                 return False
             d.add(element)
-    
+
     # general test on each column
+    column = {
+        0: [],
+        1: [],
+        2: [],
+        3: [],
+        4: [],
+        5: [],
+        6: [],
+        7: [],
+        8: [],
+        9: [],
+    }
     for row in grid:
         for id, element in enumerate(row):
-            if element != 0 and element in get_column(id, row, grid):
+            if element != 0 and element in column[id + 1]:
                 return False
-    
+            column[id + 1].append(element)
+
     # general test on sub-grid 3x3
     for row_id in range(0, 9, 3):
         for column_id in range(0, 9, 3):
             d = {}
-            sub_grid = [[], [], []]
             for i in range(3):
                 for j in range(3):
-                    sub_grid[i].append(grid[ row_id + i][column_id + j])
-            
-            for row in sub_grid:
-                for value in row:
+                    value = grid[row_id + i][column_id + j]
                     if value != 0 and value in d:
                         return False
                     d[value] = 1
-                
+
     return True
+
 
 if __name__ == "__main__":
     print("None is", check_sudoku(ill_formed))  # --> None
-    print("True is", check_sudoku(valid))       # --> True
-    print("False is", check_sudoku(invalid))    # --> False
-    print("True is", check_sudoku(easy))        # --> True
-    print("True is", check_sudoku(hard))        # --> True
-
+    print("True is", check_sudoku(valid))  # --> True
+    print("False is", check_sudoku(invalid))  # --> False
+    print("True is", check_sudoku(easy))  # --> True
+    print("True is", check_sudoku(hard))  # --> True
